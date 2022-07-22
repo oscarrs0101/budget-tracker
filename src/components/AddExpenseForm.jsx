7d88f7bd1cd03856c1a88b3a29304c0fe7c1ee6b
Oscar Rios <oscarrios0101@gmail.com>
2022-07-21 21:54:30 -0500

    funcionadlidad de la app realizada

---
+++ src/components/AddExpenseForm.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const AddExpenseForm = () => {
+  const { dispatch } = useContext(AppContext);
+  const [name, setName] = useState('');
+  const [cost, setCost] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const expense = {
+      name,
+      id: crypto.randomUUID(),
+      cost: parseInt(cost),
+    };
+
+    dispatch({
+      type: 'ADD_EXPENSE',
+      payload: expense,
+    });
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="row">
+        <div className="col-sm col-lg-4">
+          <label htmlFor="name">Name</label>
+          <input
+            required="required"
+            type="text"
+            className="form-control"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="col-sm col-lg-4">
+          <label htmlFor="cost">Cost</label>
+          <input
+            required="required"
+            type="number"
+            className="form-control"
+            id="cost"
+            value={cost}
+            onChange={(e) => setCost(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col-sm">
+          <button type="submit" className="btn btn-primary">
+            Save
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default AddExpenseForm;
